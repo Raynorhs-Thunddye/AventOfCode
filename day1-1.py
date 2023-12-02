753dae5181ab978f72660495224dbc46666acbca
Etienne TOUREAU <etienne.toureau@habiteo.com>
2023-12-02 17:52:22 +0100

    Fix hierarchy bugs.

--- day1-1.py
+++
@@ -1,3 +0,0 @@
-import re
-r=sum(int(i[re.search('[0-9]', i).span()[0]]+i[len(i)-re.search('[0-9]',i[::-1]).span()[0]-1]) for i in open('input/day1.txt','r').read().split('\n'))
-print(r) # 55834
--- day1-2.py
+++
@@ -1,9 +0,0 @@
-import re
-m={'one': 1,'two': 2,'three': 3,'four': 4,'five': 5,'six': 6,'seven': 7,'eight': 8,'nine': 9}
-p,q='[0-9]|' + '|'.join(list(m.keys())),'[0-9]|' + '|'.join(list(map(lambda x: x[::-1], list(m.keys()))))
-r=0
-for i in open('input/day1.txt','r').read().split('\n'):
- a=re.search(p, i).span()
- c=re.search(q, i[::-1]).span()
- r+=int(str(m[i[a[0]:a[1]]] if a[1]-a[0]!=1 else i[a[0]:a[1]])+str(m[i[len(i)-c[1]:len(i)-c[0]]] if c[1]-c[0]!=1 else i[len(i)-c[0]-1]))
-print(r) # 53221
